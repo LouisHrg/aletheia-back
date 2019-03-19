1e10835ace0c9efab9c5171825566c04cd5aa48d
Jonathan Rakotonirina <jrakotonirina.ext@wedigital.garden>
2019-03-19 15:10:17 +0100

    Add Docker Postgresql

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
+    'connections' => [
+        'pgsql' => [
+            'driver'    => 'pgsql',
+            'host'      => env('DB_HOST'),
+            'port'      => env('DB_PORT'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ]
+    ],
+
+    'migrations' => 'migrations',
+];
