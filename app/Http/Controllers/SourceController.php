cc6933e145ae5b1f6e0d98054e957f3a524c3092
Jonathan Rakotonirina <jonathrakoto91400@gmail.com>
2019-03-21 10:17:57 +0100

    Add action to recup content (#2)

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Source;
 
+use GuzzleHttp\Client;
+use Goutte\Client as GoutteClient;
+
 use Illuminate\Http\Request;
 
 class SourceController extends Controller
@@ -41,6 +44,36 @@ class SourceController extends Controller
     }
 
     public function content($idOzae)
+
+    {
+        $http = new Client();
+
+        $res = $http->request('GET', 'https://api.ozae.com/gnw/article/'.$idOzae.'/html_content?key='.env('OZAE_API_KEY'));
+        $data = utf8_decode(($res->getBody()));
+
+        return response($data);
+    }
+
+    public function getContentArticle($url)
+    {
+        $client = new GoutteClient();
+
+        $content = "";
+
+        $crawler = $client->request('GET', $url);
+
+        $data = $crawler->filter('p')->each(function ($node) {
+            return $node->text()."\n";
+        });
+
+        foreach($data as $d){
+            $content .= $d;
+        }
+
+        return response($content);
+    }
+
+    public function fetch(Request $request)
     {
         $http = new Client();
 
